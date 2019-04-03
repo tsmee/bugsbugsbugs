6b1c88f52a09266b5d3ad3e38020dfbcb9f15bd4
tsmee <draftpodium@gmail.com>
2019-04-03 10:15:24 +0700

    added fields and validation

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,9 +1,39 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired, Email, Length, DataRequired
+from wtforms import validators
+from wtforms.fields.html5 import EmailField, DateTimeField
+from wtforms.fields import DateField
+from flask_bootstrap import Bootstrap
 
-@app.route("/")
-def main():
-    return render_template('index.html')
+
+class MyForm(FlaskForm):
+    name = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    date = DateField('date', id='datepick', format='yyyy/mm/dd')
+    email = EmailField('Email address', [DataRequired(), validators.Email()])
+    wtf = StringField('wtf', validators=[DataRequired(), validators.Length(min=3)])
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+Bootstrap(app)
+
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = MyForm()
+    return render_template('index.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    return render_template('login.html', form=form)
+
+
 
 if __name__ == "__main__":
     app.run()
